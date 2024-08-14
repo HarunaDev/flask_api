dc5791c83568434da913dc23d3d00d03e8120545
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-14 21:06:59 +0100

    created route with http parameter

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ app = Flask(__name__)
 def index():
     return "Home"
 
+# create route with http parameter
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "John Smith",
+        "email": "john.smith@ecu.com"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+        return jsonify(user_data), 200
